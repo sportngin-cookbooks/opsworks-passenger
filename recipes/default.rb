54d8ee0c1f4acdb98a4b3f9d66dd355db7c9ff28
Brian Bergstrom <brian.bergstrom@sportngin.com>
2014-11-05 11:11:16 -0600

    Copy of definitions, recipes and templates from opsworks-puma.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,23 @@
-#
-# Cookbook Name:: opsworks-passenger
-# Recipe:: default
-#
-# Copyright (C) 2014 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+execute "gem uninstall puma" do
+  only_if "gem list | grep puma"
+end
+
+include_recipe "nginx"
+
+directory "/etc/nginx/shared"
+directory "/etc/nginx/http"
+directory "/etc/nginx/ssl"
+
+node[:deploy].each do |application, deploy|
+  puma_config application do
+    directory deploy[:deploy_to]
+    environment deploy[:rails_env]
+    logrotate deploy[:puma][:logrotate]
+    thread_min deploy[:puma][:thread_min]
+    thread_max deploy[:puma][:thread_max]
+    workers deploy[:puma][:workers]
+    worker_timeout deploy[:puma][:worker_timeout]
+    restart_timeout deploy[:puma][:restart_timeout] || 120
+  end
+end
+
