824735bcbbb2d13009a4f24fa3726e98c28ee6e5
Brian Bergstrom <brian.bergstrom@sportngin.com>
2014-11-07 16:05:11 -0600

    Fixing service and package serverspec tests.

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,14 +1,18 @@
 node[:deploy].each do |application, deploy|
+
   opsworks_deploy_user do
     deploy_data deploy
   end
-    opsworks_deploy_dir do
+
+  opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
     path deploy[:deploy_to]
   end
-  puma_web_app do
+
+  passenger_web_app do
     application application
     deploy deploy
-  end if deploy[:puma]
+  end if deploy[:passenger]
+
 end
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -2,3 +2,6 @@ require 'serverspec'
 
 # Required by serverspec
 set :backend, :exec
+
+# Fix root's PATH for serverspec resources like package and service to work.
+set :path, '/usr/local/sbin:/usr/sbin:/sbin:$PATH'
