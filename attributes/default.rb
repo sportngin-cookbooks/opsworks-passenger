7b0c08d5c42285aea2ce1efd0898b2541fba914a
Brian Bergstrom <brian.bergstrom@sportngin.com>
2014-11-10 15:25:08 -0600

    Porting over optimized gzip configs and new definiitino for custom configs in consumers.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,8 @@
 override[:nginx][:install_method] = 'custom_package'
 override[:nginx][:worker_processes] = node[:cpu][:total] * 3
 override[:nginx][:client_max_body_size] = '100M'
+override[:nginx][:gzip_http_version] = '1.1'
+override[:nginx][:gzip_comp_level] = '8'
 
 # Extensions
 default[:custom_package][:package_location] = '/usr/src/rpm/RPMS/x86_64/'
