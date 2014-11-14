289107ec906a59716b4615e51a6944639e3a8f16
Brian Bergstrom <brian.bergstrom@sportngin.com>
2014-11-14 15:31:08 -0600

    Fixing improper use of configure.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,2 @@
 include_recipe "opsworks-passenger::setup"
-include_recipe "opsworks-passenger::configure"
 include_recipe "opsworks-passenger::deploy"
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,6 +5,12 @@ gem_package "passenger" do
   not_if "gem list | egrep 'passenger \\(#{node[:passenger][:version]}'"
 end
 
+bash "Setup Nginx integration in passenger gem" do
+  code "rake nginx RELEASE=yes"
+  cwd node[:passenger][:root]
+  not_if { File.directory? "#{node[:passenger][:root]}/agents" }
+end
+
 include_recipe "opsworks-passenger::custom_package"
 
 directory node[:nginx][:dir] do
@@ -13,7 +19,7 @@ directory node[:nginx][:dir] do
   mode "0755"
 end
 
-%w{shared_server.conf.d http_server.conf.d ssl_server.conf.d}.each do |dir|
+%w{sites-available sites-enabled conf.d shared_server.conf.d http_server.conf.d ssl_server.conf.d}.each do |dir|
   directory File.join(node[:nginx][:dir], dir) do
     owner "root"
     group "root"
@@ -26,14 +32,6 @@ directory node[:nginx][:log_dir] do
   owner node[:nginx][:user]
 end
 
-%w{sites-available sites-enabled conf.d}.each do |dir|
-  directory File.join(node[:nginx][:dir], dir) do
-    owner "root"
-    group "root"
-    mode "0755"
-  end
-end
-
 %w{nxensite nxdissite}.each do |nxscript|
   template "/usr/sbin/#{nxscript}" do
     cookbook "nginx"
@@ -44,14 +42,78 @@ end
   end
 end
 
-bash "Setup Nginx integration in passenger gem" do
-  code "rake nginx RELEASE=yes"
-  cwd node[:passenger][:root]
-  not_if { File.directory? "#{node[:passenger][:root]}/agents" }
+
+template "#{node[:nginx][:dir]}/nginx.conf" do
+  cookbook "nginx"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
 end
+
+template "#{node[:nginx][:dir]}/shared_server.conf.d/shared.conf" do
+  source "nginx_shared.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+template node[:ruby_wrapper][:install_path] do
+  source "ruby-wrapper.sh.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+  variables(
+      :ruby_binary => node[:ruby_wrapper][:ruby_binary],
+      :heap_min_slots => node[:ruby_wrapper][:heap_min_slots],
+      :gc_malloc_limit => node[:ruby_wrapper][:gc_malloc_limit],
+      :heap_free_min => node[:ruby_wrapper][:heap_free_min],
+      :extra_env_vars => node[:ruby_wrapper][:extra_env_vars]
+  )
+end
+
+template "#{node[:nginx][:dir]}/conf.d/passenger.conf" do
+  source "passenger.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  variables(
+      :root => node[:passenger][:root],
+      :ruby => node[:ruby_wrapper][:install_path],
+      :config => node[:passenger]
+  )
+end
+
+template "#{node[:nginx][:dir]}/sites-available/default" do
+  source "default_site.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(
+      :root_dir => node[:nginx][:default_site][:root_dir]
+  )
+end
+if node[:nginx][:default_site][:enable]
+  execute "nxensite default" do
+    command "/usr/sbin/nxensite default"
+    not_if do
+      File.symlink?("#{node[:nginx][:dir]}/sites-enabled/default") ||
+      File.symlink?("#{node[:nginx][:dir]}/sites-enabled/000-default")
+    end
+  end
+else
+  execute "nxdissite default" do
+    command "/usr/sbin/nxdissite default"
+    only_if do
+      File.symlink?("#{node[:nginx][:dir]}/sites-enabled/default") ||
+      File.symlink?("#{node[:nginx][:dir]}/sites-enabled/000-default")
+    end
+  end
+end
+
 
 include_recipe "nginx::service"
 service "nginx" do
   action [ :enable, :start ]
 end
-
