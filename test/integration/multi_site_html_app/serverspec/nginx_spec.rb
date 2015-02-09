9492b4e3e526bd88ba382b9b962f1ef1a6f5d46b
Brian Bergstrom <brian.bergstrom@sportngin.com>
2015-02-09 14:01:49 -0600

    Adding test coverage for nginx site config.

--- test/integration/multi_site_html_app/serverspec/nginx_spec.rb
+++ test/integration/multi_site_html_app/serverspec/nginx_spec.rb
@@ -37,3 +37,63 @@ describe command('echo "127.0.0.1 tacos.org" | sudo tee -a /etc/hosts') do
   end
 end
 
+describe file('/etc/nginx/sites-enabled/test_app') do
+  its(:content) { should match <<CONF
+server {
+  listen   80 default_server;
+  server_name  localhost ip-10-153-255-120;
+  access_log  /var/log/nginx/localhost.access.log;
+
+  root   /srv/test-www/test_rack_app/current/public/;
+
+  location / {
+    if ($maintenance) {
+      return 503;
+      break;
+    }
+    try_files  $uri $uri/index.html $uri.html @app_test_app;
+  }
+  location @app_test_app {
+    # Turn on the passenger Nginx helper for this location.
+    passenger_enabled on;
+
+    # These don't seem to work in stack, which is in the http {} block
+    passenger_set_cgi_param HTTP_X_FORWARDED_FOR   $proxy_add_x_forwarded_for;
+    passenger_set_cgi_param HTTP_X_REAL_IP         $remote_addr;
+    passenger_set_cgi_param HTTP_HOST              $http_host;
+    passenger_set_cgi_param HTTP_X_FORWARDED_PROTO $scheme;
+    # https://docs.newrelic.com/docs/apm/other-features/request-queueing/request-queue-server-configuration-examples#nginx
+    passenger_set_cgi_param HTTP_X_REQUEST_START "t=${msec}";
+
+    # Rails 3.0 apps that use rack-ssl use SERVER_PORT to generate a https
+    # URL. Since internally nginx runs on a different port, the generated
+    # URL looked like this: https://host:81/ instead of https://host/
+    # By setting SERVER_PORT this is avoided.
+    passenger_set_cgi_param SERVER_PORT            80;
+
+    #
+    # Define the rack/rails application environment.
+    #
+    rack_env test;
+  }
+
+
+  error_page 503 /maintenance.html;
+  location = /maintenance.html {
+    root html;
+  }
+
+  location /nginx_status {
+    stub_status on;
+    access_log off;
+    allow 127.0.0.1;
+    deny all;
+  }
+
+  include /etc/nginx/shared_server.conf.d/*.conf;
+  include /etc/nginx/http_server.conf.d/*.conf;
+}
+
+CONF
+  }
+end
