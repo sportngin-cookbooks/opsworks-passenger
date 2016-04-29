da9cbc6caa7c7e975d63d13187a34e983e127bc2
mykola42 <mykola.myshkovskyi@gmail.com>
2016-04-29 13:42:18 -0500

    fixing Kitchen tests

--- test/integration/multi_site_html_app/serverspec/nginx_spec.rb
+++ test/integration/multi_site_html_app/serverspec/nginx_spec.rb
@@ -44,6 +44,7 @@ server {
   listen   80 default_server;
   server_name  test-kitchen.sportngin.com #{`hostname | tr -d '\n'`};
   access_log  /var/log/nginx/test-kitchen.sportngin.com.access.log main;
+  server_tokens off;
 
   root   /srv/test-www/test_rack_app/current/public/;
 
@@ -100,6 +101,7 @@ server {
   add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
   server_name  test-kitchen.sportngin.com #{`hostname | tr -d '\n'`};
   access_log  /var/log/nginx/test-kitchen.sportngin.com-ssl.access.log main;
+  server_tokens off;
 
   ssl on;
   ssl_dhparam /etc/nginx/ssl/dhparam.pem;
