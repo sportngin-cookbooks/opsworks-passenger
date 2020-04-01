0c22051266466aced77f1552eb24decef00c532d
Tim Sandquist <tim.sandquist@sportsengine.com>
2020-04-01 15:32:14 -0500

    Use passenger_ruby for finding ruby gems

--- libraries/passenger_conf.rb
+++ libraries/passenger_conf.rb
@@ -10,7 +10,7 @@ module OpsworksPassenger
       return node[:passenger][:conf][:passenger_root]
     end
 
-    ruby_gem_dir = node[:passenger][:ruby_gem_dir] || `ruby -rubygems -e 'print Gem.dir'`
+    ruby_gem_dir = node[:passenger][:ruby_gem_dir] || `"#{node[:passenger][:conf][:passenger_ruby]}" -rubygems -e 'print Gem.dir'`
     node[:passenger][:passenger_root_template] % {
       :ruby_gem_dir => ruby_gem_dir,
       :passenger_version => node[:passenger][:version]
