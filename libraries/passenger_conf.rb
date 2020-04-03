104ade0f7802a1faac0cc7d439f0d0a603bb9f6c
Tim Sandquist <tim.sandquist@sportsengine.com>
2020-04-03 09:19:29 -0500

    Use Mixin::ShellOut directly

--- libraries/passenger_conf.rb
+++ libraries/passenger_conf.rb
@@ -1,24 +1,21 @@
 module OpsworksPassenger
-  module Helper
+  module_function
 
-    include Chef::Mixin::ShellOut
+  def passenger_conf(node)
+    node[:passenger][:conf].merge(:passenger_root => expand_passenger_root(node))
+  end
 
-    def passenger_conf(node)
-      node[:passenger][:conf].merge(:passenger_root => expand_passenger_root(node))
+  def expand_passenger_root(node)
+    if node[:passenger][:conf][:passenger_root] != node.default[:passenger][:conf][:passenger_root]
+      return node[:passenger][:conf][:passenger_root]
     end
 
-    def expand_passenger_root(node)
-      if node[:passenger][:conf][:passenger_root] != node.default[:passenger][:conf][:passenger_root]
-        return node[:passenger][:conf][:passenger_root]
-      end
+    cmd = Mixlib::ShellOut.new("ruby -rubygems -e 'print Gem.dir'")
 
-      cmd = shell_out("ruby -rubygems -e 'print Gem.dir'")
-
-      ruby_gem_dir = node[:passenger][:ruby_gem_dir] || cmd.stdout
-      node[:passenger][:passenger_root_template] % {
-        :ruby_gem_dir => ruby_gem_dir,
-        :passenger_version => node[:passenger][:version]
-      }
-    end
+    ruby_gem_dir = node[:passenger][:ruby_gem_dir] || cmd.run_command.stdout
+    node[:passenger][:passenger_root_template] % {
+      :ruby_gem_dir => ruby_gem_dir,
+      :passenger_version => node[:passenger][:version]
+    }
   end
 end
