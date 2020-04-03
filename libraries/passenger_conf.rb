24403dd35b121b8de1796faa9709157c592b0ca0
Tim Sandquist <tim.sandquist@sportsengine.com>
2020-04-03 09:06:26 -0500

    Missed `end` for module Helper

--- libraries/passenger_conf.rb
+++ libraries/passenger_conf.rb
@@ -1,23 +1,24 @@
 module OpsworksPassenger
   module Helper
 
-  include Chef::Mixin::ShellOut
+    include Chef::Mixin::ShellOut
 
-  def passenger_conf(node)
-    node[:passenger][:conf].merge(:passenger_root => expand_passenger_root(node))
-  end
-
-  def expand_passenger_root(node)
-    if node[:passenger][:conf][:passenger_root] != node.default[:passenger][:conf][:passenger_root]
-      return node[:passenger][:conf][:passenger_root]
+    def passenger_conf(node)
+      node[:passenger][:conf].merge(:passenger_root => expand_passenger_root(node))
     end
 
-    cmd = shell_out("ruby -rubygems -e 'print Gem.dir'")
+    def expand_passenger_root(node)
+      if node[:passenger][:conf][:passenger_root] != node.default[:passenger][:conf][:passenger_root]
+        return node[:passenger][:conf][:passenger_root]
+      end
 
-    ruby_gem_dir = node[:passenger][:ruby_gem_dir] || cmd.stdout
-    node[:passenger][:passenger_root_template] % {
-      :ruby_gem_dir => ruby_gem_dir,
-      :passenger_version => node[:passenger][:version]
-    }
+      cmd = shell_out("ruby -rubygems -e 'print Gem.dir'")
+
+      ruby_gem_dir = node[:passenger][:ruby_gem_dir] || cmd.stdout
+      node[:passenger][:passenger_root_template] % {
+        :ruby_gem_dir => ruby_gem_dir,
+        :passenger_version => node[:passenger][:version]
+      }
+    end
   end
 end
